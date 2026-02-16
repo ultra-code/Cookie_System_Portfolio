0878d488cde78177e7de0b43ee2a210d3667ae84
ultra-code <mangobear1217@gmail.com>
2026-02-16 18:54:36 +0900

    feat: add JellyItem component with JellyId

---
+++ Assets/Scripts/JellyItem.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class JellyItem : MonoBehaviour
+{
+    public int JellyId;
+
+    private void OnValidate()
+    {
+        if (JellyId <= 0)
+        {
+            Debug.LogWarning($"[JellyItem] JellyId is invalid on {gameObject.name}");
+        }
+    }
+
+    private void Reset()
+    {
+        try
+        {
+            gameObject.tag = "Jelly";
+        }
+        catch (UnityException)
+        {
+        }
+    }
+}
